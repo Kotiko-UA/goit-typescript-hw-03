8bc8b3b0406b2e305f343b9f950b8e896194fa39
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-10-28 10:06:06 +0300

    1

--- src/1.ts
+++ src/1.ts
@@ -5,11 +5,9 @@
 */
 
 class Student {
-  constructor(public name: string, public age: number, public grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  public name: string;
+  public age: number;
+  public grade: string;
 }
 
 export {};
